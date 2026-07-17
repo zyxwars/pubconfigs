486193936a0b367ac84c1f50c0bb7f1291ed80bb
Samuel Tvrdon <43968763+zyxwars@users.noreply.github.com>
2026-07-17 20:17:44 +0200

    changes

--- symlink-configs.sh
+++ symlink-configs.sh
@@ -1,3 +1,5 @@
 ln -s "${PWD}/.vimrc" ~/.vimrc
 ln -s "${PWD}/.bash_aliases" ~/.bash_aliases
 ln -s "${PWD}/.gitconfig" ~/.gitconfig
+ln -s "${PWD}/.npmrc" ~/.npmrc
+ln -s "${PWD}/.config/pnpm/rc" ~/.config/pnpm/rc
